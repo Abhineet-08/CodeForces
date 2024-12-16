f3511ad782c35bdfaab9743788fa5cdd5c5bc57e
Abhineet-08 <113853385+Abhineet-08@users.noreply.github.com>
2024-12-16 22:11:21 +0530

    16th december

--- holidayOfEquality.cpp
+++ holidayOfEquality.cpp
@@ -1,39 +1,15 @@
-#include <iostream>
-#include <vector>
-
+#include <bits/stdc++.h>
 using namespace std;
 
-int min_charges(int n, vector<int>& welfare) {
-    int total_welfare = 0;
-    for (int w : welfare) {
-        total_welfare += w;
-    }
-    int average_welfare = total_welfare / n;
-    
-    int charges = 0;
-    for (int w : welfare) {
-        if (w < average_welfare) {
-            charges += average_welfare - w;
-        }
-    }
-    
-    return charges;
-}
-
-int main() {
-#ifndef ONLINE_JUDGE
-    freopen("input.txt", "r", stdin);
-    freopen("output.txt", "w", stdout);
-#endif    
+int main()
+{
     int n;
     cin >> n;
-    
-    vector<int> welfare(n);
-    for (int i = 0; i < n; ++i) {
-        cin >> welfare[i];
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
     }
-    
-    cout << min_charges(n, welfare)+1 << endl;
     
     return 0;
 }
